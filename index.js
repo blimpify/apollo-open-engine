d7f6f12ca1c70eb512ed8da68838cf6aac022608
Damian Lion Maring <damian.lion.maring@sap.com>
2019-05-01 23:09:13 +0200

    working with protobufjs

--- index.js
+++ index.js
@@ -1,45 +1,37 @@
 #!/usr/bin/env node
 'use strict';
 
-const zlib = require("zlib");
+
 const express = require('express');
 const app = express();
+const pino = require('pino');
+const  logger = pino({
+  useLevelLabels: true,
+  serializers: {
+    err: pino.stdSerializers.err
+  }
+});
 
-/**
- * WIP
- * reverse https://github.com/apollographql/apollo-server/blob/master/packages/apollo-engine-reporting/src/agent.ts
- */
+app.use(require('./src/router'));
 
-app.post('/api/ingress/traces', (req, res) => {
-  let rawBody = '';
-  const gunzip = zlib.createGunzip();
-  req.pipe(gunzip);
-
-  gunzip.on('data', function (data) {
-    // decompression chunk ready, add it to the buffer
-    rawBody += data;
-
-  });
-
-  gunzip.on("end", function () {
-    zlib.gunzip(rawBody, (err, data) => {
-      console.log(err, data)
-    })
-    // response and decompression complete, join the buffer and return
-    console.log(rawBody)
-
-  });
-
-  gunzip.on("error", function (err) {
-    console.log(err);
+app.use((err, req, res, next) => {
+  logger.error(err);
+  res.status(500).json({
+    message: 'Something broke!'
   });
 });
 
-app.all('*', (req, res, next) => {
-  console.log(req.path);
-  res.end();
-});
+process.on('uncaughtException', pino.final(logger, (err, finalLogger) => {
+  finalLogger.error(err, 'uncaughtException');
+  process.exit(1);
+}));
 
-app.listen(8000, function () {
-  console.log('Example app listening on port 8000!');
+process.on('unhandledRejection', pino.final(logger, (err, finalLogger) => {
+  finalLogger.error(err, 'unhandledRejection');
+  process.exit(1);
+}));
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  logger.info('App listening on port %s', port);
 });
